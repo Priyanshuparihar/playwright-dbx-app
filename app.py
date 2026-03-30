b8b1811b59cf0c054e21e7426d7fa611b43acb35
Priyanshu <ppariha6.pp@gmail.com>
2026-03-31 03:05:54 +0530

    comment out scraper functionality in app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import streamlit as st
-from scraper import scrape_quotes
+# from scraper import scrape_quotes
 
 st.title("Playwright Scraper App 🚀")
 
 if st.button("Run Scraper"):
     st.write("Running scraper...")
 
-    quotes = scrape_quotes()
+    # quotes = scrape_quotes()
 
     st.success("Done!")
 
-    for i, q in enumerate(quotes, 1):
-        st.write(f"{i}. {q}")
+    # for i, q in enumerate(quotes, 1):
+    #     st.write(f"{i}. {q}")
